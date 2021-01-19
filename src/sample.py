fb32b22a35f13c1d18716f31178c9802c353672e
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2021-01-19 20:39:58 +0900

    sample.py修正

--- src/sample.py
+++ src/sample.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-load_url = 'https://www.google.com/search?client=firefox-b-d&q=docker'
+load_url = 'https://zuma-lab.com/'
 data = requests.get(load_url)
 html = BeautifulSoup(data.content, 'html.parser')
 
-for element in html.find_all('h3'):
+for element in html.find_all('h4'):
   print(element.text)
